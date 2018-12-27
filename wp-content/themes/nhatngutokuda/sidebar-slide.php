61f3829965cefa58d7a0b162be119d44d1f092ce
DESKTOP-AQQD4TP\User <lvbao.cntt.sgu@gmail.com>
2018-12-27 11:54:11 +0700

    sua slide

--- wp-content/themes/nhatngutokuda/sidebar-slide.php
+++ wp-content/themes/nhatngutokuda/sidebar-slide.php
@@ -1,47 +1,15 @@
 <section id="slider"  class="carousel slide" data-ride="carousel">
     <div class="carousel-inner">
-        <!-- <ol class="carousel-indicators">
-            <li data-target="#slider" data-slide-to="0" class="active"></li>
-            <li data-target="#slider" data-slide-to="1"></li>
-            <li data-target="#slider" data-slide-to="2"></li>
-            <li data-target="#slider" data-slide-to="3"></li>
-            <li data-target="#slider" data-slide-to="4"></li>
-        </ol>
-        <div  class="item active">
-            <a href="/">
-                <img src="<?php echo IMG_URL."/slider1.png" ?>" alt="slider 1">
-            </a>
-        </div>
-        <div class="item">
-            <a href="/">
-                <img src="<?php echo IMG_URL."/slider2.png" ?>" alt="slider 2">
-            </a>
-        </div>
-        <div class="item">
-            <a href="/">
-                <img src="<?php echo IMG_URL."/slider3.jpg" ?>" alt="slider 3">
-            </a>
-        </div>
-        <div class="item">
-            <a href="/">
-                <img src="<?php echo IMG_URL."/slider4.jpg" ?>" alt="slider 4">
-            </a>
-        </div>
-        <div class="item">
-            <a href="/">
-                <img src="<?php echo IMG_URL."/slider5.png" ?>" alt="slider 5">
-            </a>
-        </div> -->
         <?php   global $wpdb; 
         $slides = $wpdb->get_results( "SELECT * FROM slides ", OBJECT  );?>
         <?php if( count($slides) > 0) : ?>
             <ol class="carousel-indicators">
             <?php foreach( $slides as $key => $value) :?>
-                <li data-target="#slider" data-slide-to="<?php echo $key ?>" class='<?php echo  $key==0 ? 'active' : ''  ?>'></li>
+                <li data-target="#slider" data-slide-to="<?php echo $key ?>" class='img-responsive <?php echo  $key == "0" ? "active" : ""  ?>'></li>
             <?php endforeach; ?>
             </ol>
-            <?php foreach( $slides as $slide ) :?>
-            <div class="item">
+            <?php foreach( $slides as $key2 => $slide ) :?>
+            <div class="item  <?php echo  $key2 == "0" ? "active" : ""  ?>">
                 <a href="/">
                     <img src="<?php echo  $slide->url ?>" alt="<?php echo  $slide->alt ?>">
                 </a>
